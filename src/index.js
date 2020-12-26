2931231dc668eef410eaa35e6dc26aa11fefa9d7
Sylvia <sylviaguo31@gmail.com>
2020-12-27 09:44:02 +1300

    set timestamps for user and task

--- src/index.js
+++ src/index.js
@@ -16,13 +16,3 @@ app.use(taskRouter)
 app.listen(port, ()=>{
     console.log('Server is up on port ' + port)
 })
-
-const main = async () => {
-    // const task = await Task.findById('5fe6f2d6f8708e5070d20f7f')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-    const user = await User.findById('5fe6f1a0d285a35025e628bc')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-main()
