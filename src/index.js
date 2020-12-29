df1954ee77370c6f4ae89af6a0931b64148a15f9
Sylvia <sylviaguo31@gmail.com>
2020-12-30 12:23:33 +1300

    get avatar, resize and convert avatar

--- src/index.js
+++ src/index.js
@@ -1,38 +1,14 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 const userRouter = require('../src/routers/user')
 const taskRouter = require('../src/routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-
-const multer = require('multer')
-const upload = multer({
-    dest:'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, callback){
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return callback(new Error('Please upload a world!'))
-        }
-        callback(undefined, true)
-    }
-})
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({error: error.message})
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
-
 
 app.listen(port, ()=>{
     console.log('Server is up on port ' + port)
